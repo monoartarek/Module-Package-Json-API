09172fdb4b286796452f3c71dad629df198b81dd
monoartarek <infinitymail2024@gmail.com>
2025-08-02 01:31:08 +0600

    me

--- prac.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-response = requests.get("https://jsonplaceholder.typicode.com/posts")
-
-print(response.status_code)     # Should print 200
-        # Will print the actual list of posts in JSON
